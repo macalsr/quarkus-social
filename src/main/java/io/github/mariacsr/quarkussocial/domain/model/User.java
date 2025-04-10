e5b26306fcab1393a061b16bd8798d1401c80763
Maria <mmariasribeiro@gmail.com>
2025-04-09 22:20:29 -0300

    feat/ crud usuarios

--- src/main/java/io/github/mariacsr/quarkussocial/domain/model/User.java
+++ src/main/java/io/github/mariacsr/quarkussocial/domain/model/User.java
@@ -1,7 +1,6 @@
 package io.github.mariacsr.quarkussocial.domain.model;
 
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 import lombok.*;
--- src/main/java/io/github/mariacsr/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/mariacsr/quarkussocial/rest/UserResource.java
@@ -26,9 +26,34 @@ public class UserResource {
     }
 
     @GET
+    @Transactional
     public Response listAllUsers() {
         PanacheQuery<PanacheEntityBase> query = User.findAll();
         return Response.ok(query.list()).build();
     }
 
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id) {
+      User user = User.findById(id);
+      if(user != null) {
+          user.delete();
+          return Response.ok().build();
+      }
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userRequest) {
+        User user = User.findById(id);
+        if(user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+            user.setName(userRequest.name());
+            user.setAge(userRequest.age());
+        return Response.ok(user).build();                       
+    }
 }
