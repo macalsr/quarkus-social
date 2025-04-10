2b14f04772dc5e10c8c6c197610247a9c325b0e6
Maria <mmariasribeiro@gmail.com>
2025-04-09 23:09:04 -0300

    feat/ criando repositories e implementando validator.

--- src/main/java/io/github/mariacsr/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/mariacsr/quarkussocial/rest/UserResource.java
@@ -1,34 +1,56 @@
 package io.github.mariacsr.quarkussocial.rest;
 
 import io.github.mariacsr.quarkussocial.domain.model.User;
+import io.github.mariacsr.quarkussocial.domain.reposity.UserRepository;
 import io.github.mariacsr.quarkussocial.rest.dto.CreateUserRequest;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.github.mariacsr.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Set;
 
 @Path("/users")
 public class UserResource {
 
+    private final UserRepository repository;
+
+    private final Validator validator;
+
+    @Inject
+    public UserResource(UserRepository repository, Validator validator) {
+        this.repository = repository;
+        this.validator = validator;
+    }
+
     @POST
     @Transactional
     public Response createUser(CreateUserRequest userRequest) {
+
+        Set<ConstraintViolation<CreateUserRequest>> constraintViolations = validator.validate(userRequest);
+
+        if(!constraintViolations.isEmpty()){
+            return ResponseError.createFromValidation(constraintViolations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
+        }
 
         User user = User.builder()
                 .age(userRequest.age())
                 .name(userRequest.name())
                 .build();
 
-        user.persist();
-
-        return Response.ok(user).build();
+        repository.persist(user);
+        return Response.status(Response.Status.CREATED).entity(user).build();
     }
 
     @GET
     @Transactional
     public Response listAllUsers() {
-        PanacheQuery<PanacheEntityBase> query = User.findAll();
+        PanacheQuery<User> query = repository.findAll();
         return Response.ok(query.list()).build();
     }
 
@@ -36,21 +58,25 @@ public class UserResource {
     @Path("{id}")
     @Transactional
     public Response deleteUser(@PathParam("id") Long id) {
-      User user = User.findById(id);
+      User user = repository.findById(id);
       if(user != null) {
-          user.delete();
+          repository.delete(user);
           return Response.ok().build();
       }
-      return Response.status(Response.Status.NOT_FOUND).build();
+      return Response.status(Response.Status.NO_CONTENT).build();
     }
     
     @PUT
     @Path("{id}")
     @Transactional
     public Response updateUser(@PathParam("id") Long id, CreateUserRequest userRequest) {
-        User user = User.findById(id);
+        Set<ConstraintViolation<CreateUserRequest>> constraintViolations = validator.validate(userRequest);
+        if(!constraintViolations.isEmpty()){
+            return ResponseError.createFromValidation(constraintViolations).withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
+        }
+        User user = repository.findById(id);
         if(user == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
+            return Response.status(Response.Status.NO_CONTENT).build();
         }
             user.setName(userRequest.name());
             user.setAge(userRequest.age());
--- src/main/java/io/github/mariacsr/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/mariacsr/quarkussocial/rest/dto/CreateUserRequest.java
@@ -1,6 +1,8 @@
 package io.github.mariacsr.quarkussocial.rest.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CreateUserRequest(String name, Integer age) {
+public record CreateUserRequest(@NotBlank(message = "Name is Required") String name, @NotNull(message = "Age is Required") Integer age) {
 }
