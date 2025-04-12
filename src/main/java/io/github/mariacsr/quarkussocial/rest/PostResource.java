a2fc4405ceb022672d02fd28351a38a6ed4c6b5a
Maria <mmariasribeiro@gmail.com>
2025-04-12 09:47:08 -0300

    feat/ Criando resources de posts e endpoints

--- src/main/java/io/github/mariacsr/quarkussocial/rest/PostResource.java
+++ src/main/java/io/github/mariacsr/quarkussocial/rest/PostResource.java
@@ -1,21 +1,56 @@
 package io.github.mariacsr.quarkussocial.rest;
 
+import io.github.mariacsr.quarkussocial.domain.model.Post;
+import io.github.mariacsr.quarkussocial.domain.model.User;
+import io.github.mariacsr.quarkussocial.domain.repository.PostRepository;
+import io.github.mariacsr.quarkussocial.domain.repository.UserRepository;
+import io.github.mariacsr.quarkussocial.rest.dto.CreatePostRequest;
+import io.github.mariacsr.quarkussocial.rest.dto.PostResponse;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.List;
 
 @Path("/users/{userId}/posts")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResource {
 
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    @Inject
+    public PostResource(UserRepository userRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+    }
+
     @POST
-    public Response savePost(){
-        return Response.status(Response.Status.CREATED).build();
+    @Transactional
+    public Response savePost(@PathParam("userId") Long userId, CreatePostRequest createPostRequest) {
+        User user = userRepository.findById(userId);
+        if(user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        Post post = new Post();
+        post.setText(createPostRequest.text());
+        post.setUser(user);
+        postRepository.persist(post);
+        return Response.status(Response.Status.CREATED).entity(post).build();
     }
 
     @GET
-    public Response getPosts(){
-        return Response.status(Response.Status.OK).build();
+    public Response getPosts(@PathParam("userId") Long userId){
+        User user = userRepository.findById(userId);
+        if(user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        List<Post> postList = postRepository.findPostsByUserOrderByDate(user);
+        List<PostResponse> postResponseList = postList.stream().map(PostResponse::of).toList();
+        return Response.status(Response.Status.OK)
+                .entity(postResponseList).build();
     }
 }
---
+++ src/main/java/io/github/mariacsr/quarkussocial/rest/dto/CreatePostRequest.java
@@ -0,0 +1,4 @@
+package io.github.mariacsr.quarkussocial.rest.dto;
+
+
+public record CreatePostRequest(String text){}
